57a3e6e2c6fd239aa79fbade0734c4a7593ebaaa
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-07-05 10:41:19 +0200

    feat: api for button component

---
+++ packages/ui/src/components/atoms/ZButton/ZButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <component
+    :is="tag"
+    class="z-button"
+    v-on="$listeners"
+  >
+    <slot />
+  </component>
+</template>
+<script>
+  export default {
+    name: "ZButton",
+    props: {
+      tag: {
+        type: String,
+        default: "button"
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .z-button {}
+</style>
