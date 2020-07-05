8166bdec8bc5c30bcf63d3508e81d9dc72777894
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-07-05 18:17:45 +0200

    feat: api for banner component

---
+++ packages/ui/src/components/molecules/ZBanner/ZBanner.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+import ZBanner from "./ZBanner.vue";
+describe("ZBanner.vue", () => {
+  it("renders a component", () => {
+    const component = shallowMount(ZBanner);
+    expect(component.contains(".z-banner")).toBe(true);
+  });
+});
---
+++ packages/ui/src/components/molecules/ZBanner/ZBanner.vue
@@ -0,0 +1,69 @@
+<template>
+  <component
+    :is="tag"
+    class="z-banner"
+  >
+    <slot name="title">
+      <ZHeading>{{ title }}</ZHeading>
+    </slot>
+    <slot name="content">
+      <ZText>{{ content }}</ZText>
+    </slot>
+    <slot name="callToAction">
+      <ZLink v-for="(callToAction, key) in callToActions" :key="key" :to="callToAction.to">
+        {{ callToAction.name }}
+      </ZLink>
+    </slot>
+    <slot name="thumb">
+      <ZImage
+        :src="srcImage"
+        :sources="sourcesImage"
+        :alt="title"
+      />
+    </slot>
+  </component>
+</template>
+<script>
+  import ZHeading from "../../atoms/ZHeading/ZHeading";
+  import ZText from "../../atoms/ZText/ZText";
+  import ZLink from "../../atoms/ZLink/ZLink";
+  import ZImage from "../../atoms/ZImage/ZImage";
+  export default {
+    name: "ZBanner",
+    components: {
+      ZHeading,
+      ZText,
+      ZLink,
+      ZImage
+    },
+    props: {
+      tag: {
+        type: String,
+        default: "div"
+      },
+      title: {
+        type: String,
+        default: ""
+      },
+      content: {
+        type: String,
+        default: ""
+      },
+      callToActions: {
+        type: Array,
+        default: ()=>([])
+      },
+      srcImage: {
+        type: String,
+        default: ""
+      },
+      sourcesImage: {
+        type: Array,
+        default: ()=>([])
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .z-banner {}
+</style>
