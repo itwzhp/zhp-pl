c8f8dbdd98abf6a86cdad14e27dc7d377c32eab0
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-07-05 18:42:21 +0200

    feat: api for input component

---
+++ packages/ui/src/components/atoms/ZInput/ZInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <input
+    v-model="state"
+    class="z-input"
+    :type="type"
+    :value="value"
+  >
+</template>
+<script>
+  export default {
+    name: "ZInput",
+    model: {
+      prop: "input"
+    },
+    props: {
+      input: {
+        type: [String, Number, Boolean],
+        default: ""
+      },
+      value: {
+        type: [String, Number, Boolean],
+        default: ""
+      },
+      type: {
+        type: String,
+        default: "text"
+      }
+    },
+    data(){
+      return {
+        state: this.input
+      }
+    },
+    watch: {
+      state(value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .z-input {}
+</style>
