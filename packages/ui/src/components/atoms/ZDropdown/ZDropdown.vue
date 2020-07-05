66fefaadf62a132100f8d2afebfee2d1970647ce
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-07-05 10:50:35 +0200

    feat: api for dropdown component

---
+++ packages/ui/src/components/atoms/ZDropdown/ZDropdown.vue
@@ -0,0 +1,22 @@
+<template>
+  <component
+    :is="tag"
+    class="z-dropdown"
+  >
+    <slot />
+  </component>
+</template>
+<script>
+  export default {
+    name: "ZDropdown",
+    props: {
+      tag: {
+        type: String,
+        default: 'div'
+      }
+    },
+  }
+</script>
+<style lang="scss">
+  .z-dropdown {}
+</style>
