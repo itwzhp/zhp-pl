165f5642ef15ca05d90f29e931f0364fcd784eda
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-08-30 15:24:09 +0200

    refactor: remove card component

--- packages/ui/src/components/molecules/ZCard/ZCard.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import ZCard from './ZCard.vue';
-
-describe('ZCard.vue', () => {
-  it('renders a component', () => {
-    const component = shallowMount(ZCard);
-    expect(component.contains('.z-card')).toBe(true);
-  });
-});
