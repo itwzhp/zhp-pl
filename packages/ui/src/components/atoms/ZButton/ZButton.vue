a4561980d7ea5ee7608470955052e615d498b1bb
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-08-31 10:59:53 +0200

    feat: update styles for button

--- packages/ui/src/components/atoms/ZButton/ZButton.vue
+++ packages/ui/src/components/atoms/ZButton/ZButton.vue
@@ -23,11 +23,14 @@ export default {
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    border: 0;
-    color: var(--button-color);
+    border: 1px solid #7ba22e;
+    background: var(--button-background, #a6ce39);
+    border-radius: 6px;
+    color: var(--button-color, #fffff);
 
     &--text {
       padding: 0;
+      border: 0;
       background: transparent;
     }
   }
