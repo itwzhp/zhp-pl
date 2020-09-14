7b10058f9374d2ff233cfe46cf7a08bfc6125d99
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-09-14 21:13:39 +0200

    feat: add component for form

---
+++ packages/ui/src/components/organisms/ZForm/ZForm.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+import ZForm from "./ZForm.vue";
+describe("ZForm.vue", () => {
+  it("renders a component", () => {
+    const component = shallowMount(ZForm);
+    expect(component.get(".z-form"));
+  });
+});
---
+++ packages/ui/src/components/organisms/ZForm/ZForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="tag" class="z-form"></component>
+</template>
+
+<script>
+  export default {
+    name: "ZForm",
+    props: {
+      tag: {
+        type: String,
+        default: 'div'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .z-form {}
+</style>
