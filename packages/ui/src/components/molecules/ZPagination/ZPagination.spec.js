5c16ac5e69e6da433f4fb14b346ab4ba8c3c0941
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-09-14 21:16:36 +0200

    feat: pagination component

---
+++ packages/ui/src/components/molecules/ZPagination/ZPagination.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+import ZPagination from "./ZPagination.vue";
+describe("ZPagination.vue", () => {
+  it("renders a component", () => {
+    const component = shallowMount(ZPagination);
+    expect(component.get(".z-pagination"));
+  });
+});
---
+++ packages/ui/src/components/molecules/ZPagination/ZPagination.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="tag" class="z-pagination"></component>
+</template>
+
+<script>
+  export default {
+    name: "ZPagination",
+    props: {
+      tag: {
+        type: String,
+        default: 'div'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .z-pagination {}
+</style>
