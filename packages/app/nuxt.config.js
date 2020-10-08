682a8653c98758d0dee6a7e212387b0fd0f2554f
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-10-08 18:16:23 +0200

    fix: favicons in nuxt

--- packages/app/nuxt.config.js
+++ packages/app/nuxt.config.js
@@ -23,7 +23,11 @@ export default {
       { rel: 'stylesheet', href: 'https://use.typekit.net/lqw5sjc.css' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/16.png' },
+      { rel: 'apple-touch-icon', href: '/144.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/114.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/144.png' }
     ]
   },
   /*
