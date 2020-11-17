23210e4d4fcb91d3249d95850bc4248e9496fd81
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-11-17 16:36:44 +0100

    feat: vue-fragment

--- packages/app/nuxt.config.js
+++ packages/app/nuxt.config.js
@@ -35,8 +35,7 @@ export default {
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
-  plugins: [
-  ],
+  plugins: ['~plugins/fragment'],
   /*
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
---
+++ packages/app/plugins/fragment
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import { Fragment } from 'vue-fragment'
+Vue.component('Fragment', Fragment)
