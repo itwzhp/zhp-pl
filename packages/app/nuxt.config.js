189e12236e0f7f308571c0fdb6984d780c787777
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2021-03-21 10:33:23 +0100

    chore: clear nuxt config for wordpress theme

--- packages/app/nuxt.config.js
+++ packages/app/nuxt.config.js
@@ -1,16 +1,7 @@
 import { sortRoutes } from '@nuxt/utils'
 export default {
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+  target: 'static',
   head: {
-    title: 'Związek Harcerstwa Polskiego',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -22,51 +13,13 @@ export default {
       { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
-  /*
-  ** Global CSS
-  */
   css: [
     '@zhp-pl/ui/src/styles/styles.scss'
   ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: ['~plugins/fragment'],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
-  components: true,
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // '@nuxtjs/eslint-module',
-    // '@nuxtjs/stylelint-module',
-    '@nuxtjs/pwa',
-    '@nuxtjs/google-analytics'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/sentry'
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-    baseURL: process.env.REST_API
-  },
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
+  plugins: [],
+  buildModules: [],
+  modules: ['@nuxtjs/axios'],
+  axios: { baseURL: process.env.REST_API },
   build: {
     extend (config, { isClient }) {
       config.resolve.alias = {
@@ -77,17 +30,6 @@ export default {
     transpile: [/^@zhp-pl/],
     publicPath: '/wp-content/themes/zhp-pl/_nuxt'
   },
-  pwa: {
-    meta: {
-      name: 'Związek Harcerstwa Polskiego',
-      theme_color: '#78a22f'
-    },
-    manifest: {
-      name: 'Związek Harcerstwa Polskiego',
-      lang: 'pl',
-      useWebmanifestExtension: false
-    }
-  },
   publicRuntimeConfig: {},
   router: {
     extendRoutes (routes, resolve) {
@@ -95,18 +37,6 @@ export default {
         name: 'custom',
         path: '*',
         component: resolve(__dirname, 'pages/404.vue')
-      })
-      routes.push({
-        name: 'dotacje-slug',
-        path: '/dotacje/:slug',
-        component: resolve(__dirname, 'pages/_slug.vue'),
-        children: [
-          {
-            path: ':id',
-            component: resolve(__dirname, 'pages/_slug/_id.vue'),
-            name: 'dotacje-id'
-          }
-        ]
       })
       const year = new Date().getFullYear()
       for (let y = 2010; y <= year; y++) {
@@ -130,13 +60,4 @@ export default {
     subFolders: false,
     fallback: false
   }
-  // googleAnalytics: {
-  //   id: process.env.GA
-  // },
-  // sentry: {
-  //   dsn: 'https://dbc6503ec4004c37a3f7706daa89aa73@o479654.ingest.sentry.io/5524988',
-  //   config: {
-  //     environment: process.env.NODE_ENV
-  //   }
-  // },
 }
