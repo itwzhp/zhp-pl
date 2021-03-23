c064e76d47244ccab8c61c5eaeb0d180a4aa594d
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2021-03-23 20:05:57 +0100

    feat(theme): add ACF to theme

--- packages/theme/functions.php
+++ packages/theme/functions.php
@@ -7,6 +7,24 @@ $updateChecker = Puc_v4_Factory::buildUpdateChecker(
     'zhp-pl'
 );
 // ACF
+// Define path and URL to the ACF plugin.
+define( 'MY_ACF_PATH', get_stylesheet_directory() . '/includes/acf/' );
+define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/includes/acf/' );
+
+// Include the ACF plugin.
+include_once( MY_ACF_PATH . 'acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return MY_ACF_URL;
+}
+
+// (Optional) Hide the ACF admin menu item.
+add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+function my_acf_settings_show_admin( $show_admin ) {
+    return false;
+}
 if( function_exists('acf_add_local_field_group') ):
 
     acf_add_local_field_group(array(
