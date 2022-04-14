46a28e52ce1a42cd14423ea43b39df87089860e6
writ3it <git@writ3it.tech>
2022-04-14 21:18:30 +0200

    Deployment from theme branch.

--- packages/theme/functions.php
+++ packages/theme/functions.php
@@ -11,8 +11,10 @@ $updateChecker = Puc_v4_Factory::buildUpdateChecker(
 define( 'MY_ACF_PATH', get_stylesheet_directory() . '/includes/acf/' );
 define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/includes/acf/' );
 
-// Include the ACF plugin.
-include_once( MY_ACF_PATH . 'acf.php' );
+// Require ACF plugin
+if (!class_exists('ACF')) {
+    throw new Exception('ACF plugin not activated');
+}
 
 // Customize the url setting to fix incorrect asset URLs.
 add_filter('acf/settings/url', 'my_acf_settings_url');
