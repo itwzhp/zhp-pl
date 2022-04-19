57a5ca09bb680a84aaed8e0188f251abadb2c7d1
writ3it <git@writ3it.tech>
2022-04-19 17:34:29 +0200

    Wordpress theme publishing via Github Releases added.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Nowa ZHP
+[![Build theme for WP sites](https://github.com/zhp-wlkp/zhp-pl/actions/workflows/build.yml/badge.svg)](https://github.com/zhp-wlkp/zhp-pl/actions/workflows/build.yml)
+
 **Current status:** Alpha
 
 Project for [The Polish Scouting and Guiding Association (ZHP)](https://zhp.pl/) base on Vue.js UI library, Nuxt.js App and WordPress Integration for content.
--- packages/theme/functions.php
+++ packages/theme/functions.php
@@ -2,7 +2,7 @@
 // https://github.com/YahnisElsts/plugin-update-checker
 require 'plugin-update-checker-4.11/plugin-update-checker.php';
 $updateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'http://wordpress.przemyslawspaczek.pl/details.json',
+    '{UPDATE_DETAILS_URL}',
     __FILE__,
     'zhp-pl'
 );
@@ -11,8 +11,10 @@ $updateChecker = Puc_v4_Factory::buildUpdateChecker(
 define( 'MY_ACF_PATH', get_stylesheet_directory() . '/includes/acf/' );
 define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/includes/acf/' );
 
-// Include the ACF plugin.
-include_once( MY_ACF_PATH . 'acf.php' );
+// Require ACF plugin
+if (!class_exists('ACF')) {
+    include_once( MY_ACF_PATH . 'acf.php' );
+}
 
 // Customize the url setting to fix incorrect asset URLs.
 add_filter('acf/settings/url', 'my_acf_settings_url');
